96c74e74840142ad9ce677b43b2c66cf5a8dd460
Axel-Newton <axel.larsson@edu.newton.se>
2025-10-02 11:30:29 +0200

    asdasd

--- testgit/Program.cs
+++ testgit/Program.cs
@@ -5,12 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, Worlddddd!");
-        Console.ReadKey();
+        //Console.ReadKey();
         Console.WriteLine("Testttttttt222");
 
         int x = 10; 
         int y = 20;
         int z = x + y;
         Console.WriteLine(x + y);
+        Console.WriteLine(x + y + 2);
     }
 }
